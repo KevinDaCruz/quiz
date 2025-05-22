9bdf3cb818a56ac440e938d168343f7e11080d7c
Kevin <kevindc39@hotmail.com>
2025-05-22 16:16:30 +0200

    Ajout d'un texte dans FilterQuiz.jsx

--- src/components/FilterQuiz.jsx
+++ src/components/FilterQuiz.jsx
@@ -2,16 +2,18 @@ import { Form } from "react-bootstrap";
 
 const FilterQuiz = ({ category, setCategory }) => {
   return (
-    <Form.Select
-      value={category}
-      onChange={(e) => setCategory(e.target.value)}
-      className="mb-3"
-    >
-      <option value="Toutes">Toutes</option>
-      <option value="Math">Math</option>
-      <option value="Science">Science</option>
-      <option value="Histoire">Histoire</option>
-    </Form.Select>
+    <div className="mb-3">
+      <p>Filtrer par catégorie</p>
+      <Form.Select
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value="Toutes">Toutes</option>
+        <option value="Math">Math</option>
+        <option value="Science">Science</option>
+        <option value="Histoire">Histoire</option>
+      </Form.Select>
+    </div>
   );
 };
 
